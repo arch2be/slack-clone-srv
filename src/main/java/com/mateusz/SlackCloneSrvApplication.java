3dfc471464f8031e0416ab639c69ea328c0314e2
MateuszP21 <jdvozef@gmail.com>
2020-09-20 21:39:10 +0200

    Initial commit

---
+++ src/main/java/com/mateusz/SlackCloneSrvApplication.java
@@ -0,0 +1,13 @@
+package com.mateusz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackCloneSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackCloneSrvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateusz/SlackCloneSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackCloneSrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
